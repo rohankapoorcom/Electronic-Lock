f9a706df8be61a3d4d580b971f0ea2aaa91bd1ca
Rohan Kapoor <rohan@rohankapoor.com>
2014-04-28 20:41:12 -0500

    Trying to use GPIO events instead of polling

--- ElectronicLock.py
+++ ElectronicLock.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
 from ouimeaux.environment import Environment
+
 import os
 import time
+import thread
 
 # Setup GPIO using BCM Numbering
 GPIO.setmode(GPIO.BCM)
@@ -19,22 +21,25 @@ switch = env.get_switch('eolamp')
 
 def lock_unlock(state):
 	if not state:
-		os.system("./lock.py")
+		os.system("./lock.py &")
 	else:
-		os.system("./unlock.py")
+		os.system("./unlock.py &")
 	return
 
-while True:
-	GPIO.wait_for_edge(buttonPin, GPIO.FALLING)
+def toggle_lights():
+	ret = switch.basicevent.GetBinaryState()
+	# SBS takes a weird dict for some reason. Invert.
+	ret['BinaryState'] = str(int(not int(ret['BinaryState'])))
+	switch.basicevent.SetBinaryState(**ret)
+
+def button_pushed(channel):
+	global currentState
+	print "Big Red Button pressed"
 	timeStarted = time.time()
-	print("Big Red Button pressed")
-#	GPIO.wait_for_edge(buttonPin, GPIO.RISING)
 	while GPIO.input(buttonPin) == 0 and time.time() - timeStarted < 0.5:
-		# Wait some small amount of time.
-		# time.sleep(0.01)
 		pass
 	timeEnded = time.time()
-	print("Big Red Button released")
+	print "Big Red Button released"
 
 	duration = timeEnded - timeStarted
 	if (duration >= 0.5):
@@ -42,11 +47,14 @@ while True:
 		currentState = not currentState
 	else:
 		# Handle wemo toggling
-		ret = switch.basicevent.GetBinaryState()
-		# SBS takes a weird dict for some reason. Invert.
-		ret['BinaryState'] = str(int(not int(ret['BinaryState'])))
-		switch.basicevent.SetBinaryState(**ret)
+		try:
+			thread.start_new_thread(toggle_lights)
+		except:
+			print "Error: unable to start thread"
 
-		time.sleep(0.01)
 
+GPIO.add_event_detect(buttonPin, GPIO.RISING, callback=button_pushed, bouncetime=500)
+while True:
+	pass
+	
 GPIO.cleanup()
